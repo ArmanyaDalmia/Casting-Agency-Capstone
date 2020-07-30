5f4114bc7472173891c275bc8a499a1cb6f9eb63
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-30 02:28:09 -0400

    Update environment variables for Auth0

--- auth.py
+++ auth.py
@@ -1,13 +1,14 @@
 import json
+import os
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'fsndauthorization.us.auth0.com'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'drinks'
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 
 
--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 export DATABASE_URL='postgres://armanyadalmia:armanyadalmia@localhost:5433/casting_agency'
+
+export AUTH0_DOMAIN='fsndauthorization.us.auth0.com'
+export API_AUDIENCE='agency'
