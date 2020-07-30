b61fe4183d474a1c92b83c11a9da6db3e8e9b35b
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-30 00:50:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from auth import AuthError, requires_auth
+from models import *
+
 def create_app(test_config=None):
-  # create and configure the app
+
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
