822c9c69fe3b85ebdff046dd1443c9232d3eaa15
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-30 17:17:08 -0400

    Update env var for db

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 export DATABASE_URL='postgres://armanyadalmia:armanyadalmia@localhost:5433/casting_agency'
-export TEST_DATABASE_URL='postgres://armanyadalmia:armanyadalmia@localhost:5433/casting_agency_test'
-
+export TEST_DATABASE_URL='postgres://armanyadalmia:armanyadalmia@localhost:5433/test_casting_agency'
 
 export AUTH0_DOMAIN='fsndauthorization.us.auth0.com'
 export API_AUDIENCE='agency'
